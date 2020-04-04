04612e6624b6a471462de6dda8ac7d926aad78a9
Affan100 <affan_patan@hotmail.com>
2020-04-04 16:44:02 +0700

    commitedit

--- index.js
+++ index.js
@@ -7,54 +7,60 @@ let app = express();
 var request = require('request');
 
 
-
 app.use(cors({ origin: ['http://localhost:3000'], methods: ['GET', 'POST', 'PUT', 'DELETE'], credentials: true }));
+
 /* session*/
 app.use(session({
     secret: 'keyboard cat', cookie: { maxAge: 60000 },
     resave: false, saveUninitialized: false
 }))
+
 // all of our routes will be prefixed with /api
 app.use('/api', bodyParser.json(), router);   //[use json]
 app.use('/api', bodyParser.urlencoded({ extended: false }), router);
-let bears = [
-    { 'id': 0, 'name': 'Latin', 'weight': 211, 'img': 'https://lumiere-a.akamaihd.net/v1/images/c94eed56a5e84479a2939c9172434567c0147d4f.jpeg?region=0,0,600,600' },
-    { 'id': 1, 'name': 'vinnie', 'weight': 111, 'img': 'https://cdn2.mhpbooks.com/2018/07/winnie_pooh_PNG37592.png' },
+
+let products = [
+    {
+        'id': 0, 'Productname': 'Mask', 'price': 300, 'img': 'https://scontent.fhdy4-1.fna.fbcdn.net/v/t1.0-9/90188733_2504026246477812_2792406922031005696_o.jpg?_nc_cat=101&_nc_sid=110474&_nc_eui2=AeGzHVIjCOv0byztGAw-RLXDuOf3Zavebae45_dlq95tpwiTGcQWz18HRZrZhEoSJ0RjvfzgANnARg6KX8krpgZb&_nc_oc=AQmQLUY1Szmt2DAYP9BKD-xSdYf6hBOOaQ8tefik__KgGxqUTOlqGEdlQGLfPTX7_bQ&_nc_ht=scontent.fhdy4-1.fna&oh=e151e78f9549b6f607660e475ef8e72b&oe=5EAE696E'
+    },
+    {
+        'id': 1, 'Productname': 'Compression fabric', 'price': 200, 'img': 'https://scontent.fhdy4-1.fna.fbcdn.net/v/t1.0-9/11295688_1616882231858889_3733436626993473123_n.jpg?_nc_cat=109&_nc_sid=dd9801&_nc_eui2=AeF2r3cUxU722YElM85BABJcg5AAZtcSpZKDkABm1xKlkjM1q8auBJ7xF4pEZjHEVvQ0XEMFGAA3DFzp_pcLTWbY&_nc_oc=AQlo3kQG2IBZbDmSFibdo22i1f3wYSTkk-KgVczdx4AxbjBRKG8X854_8VlJOToZ47o&_nc_ht=scontent.fhdy4-1.fna&oh=4b13583e1e275defcecae66c3f8ca6f4&oe=5EACD0CC'
+    },
 ];
 
-router.route('/bears')
+router.route('/product')
     // get all bears
-    .get((req, res) => res.json(bears))
+    .get((req, res) => res.json(products))
     // insert a new bear
     .post((req, res) => {
-        var bear = {};
-        bear.id = bears.length > 0 ? bears[bears.length - 1].id + 1 : 0;
-        bear.name = req.body.name
-        bear.weight = req.body.weight
-        bear.img = req.body.img
-        bears.push(bear);
-        res.json({ message: 'Bear created!' })
+        var product = {};
+        product.id = products.length > 0 ? products[products.length - 1].id + 1 : 0;
+        product.name = req.body.name
+        product.weight = req.body.weight
+        product.img = req.body.img
+        products.push(product);
+        res.json({ message: 'Product created!' })
     })
-router.route('/bears/:bear_id')
+router.route('/product/:product_id')
     .get((req, res) => {
-        let id = req.params.bear_id
-        let index = bears.findIndex(bear => (bear.id === +id))
-        res.json(bears[index])                   // get a bear
+        let id = req.params.product_id
+        let index = products.findIndex(product => (product.id === +id))
+        res.json(products[index])                   // get a bear
     })
     .put((req, res) => {                               // Update a bear
-        let id = req.params.bear_id
-        let index = bears.findIndex(bear => (bear.id === +id))
-        bears[index].name = req.body.name;
-        bears[index].weight = req.body.weight;
-        bears[index].img = req.body.img;
-        res.json({ message: 'Bear updated!' + req.params.bear_id });
+        let id = req.params.product_id
+        let index = products.findIndex(product => (product.id === +id))
+        products[index].name = req.body.name;
+        products[index].weight = req.body.weight;
+        products[index].img = req.body.img;
+        res.json({ message: 'Product updated!' + req.params.product_id });
     })
     .delete((req, res) => {                   // Delete a bear
         // delete     bears[req.params.bear_id]
-        let id = req.params.bear_id
-        let index = bears.findIndex(bear => bear.id === +id)
-        bears.splice(index, 1)
-        res.json({ message: 'Bear deleted: ' + req.params.bear_id });
+        let id = req.params.product_id
+        let index = products.findIndex(product => product.id === +id)
+        products.splice(index, 1)
+        res.json({ message: 'Product deleted: ' + req.params.product_id });
     })
 
 
